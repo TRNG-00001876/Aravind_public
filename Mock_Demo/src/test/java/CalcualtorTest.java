901fd58dd2fe9f474dc0aabab76fadcec0ff1cb2
AravindBetharasi <kaveri,priya@revature.com>
2024-05-04 12:25:48 +0530

    Mockito files added

---
+++ Mock_Demo/src/test/java/CalcualtorTest.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+
+public class CalcualtorTest {
+    @Test
+    public void testAddition() {
+        Calculator mockedCalculator = mock(Calculator.class);
+        when(mockedCalculator.add(2, 3)).thenReturn(5);
+
+        assertEquals(5, mockedCalculator.add(2, 3));
+
+    }
+
+    @Test
+    public void testSubtraction() {
+        Calculator mockedCalculator = mock(Calculator.class);
+        when(mockedCalculator.subtract(5, 3)).thenReturn(2);
+
+        assertEquals(2, mockedCalculator.subtract(5, 3));
+
+    }
+
+}
